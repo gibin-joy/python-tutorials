0ffd7e83f97bcf83a5ed1218c302b2b89c5b4e87
gibin-joy <gibinjoy2001@gmail.com>
2024-05-17 09:17:14 +0530

    hand_tracking

--- hand_tracking/hand_tracking.py
+++ hand_tracking/hand_tracking.py
@@ -1,13 +1,30 @@
 import mediapipe as mp
 import cv2
 
-img = cv2.imread('hand_tracking/EyhYUTcWQAAloVc.jpeg')
-converted =cv2.cvtColor(img,cv2.COLOR_BGR2RGB)   #media pi[e cant read BGR, only process RGB
+img = cv2.imread('hand_tracking/0_0Dk9Foiam83oGGrl.jpg')
+
+rgb_img=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)   #media pipe cant read BGR, only process RGB
+
 mp_hands=mp.solutions.hands
+mp_drawings=mp.solutions.drawing_utils
+
 hands= mp_hands.Hands(static_image_mode=True,max_num_hands=2)
 
-result=hands.process(img)
+result=hands.process(rgb_img)
+
 print(result.multi_handedness)
+
+print(result.multi_hand_landmarks)
+
+
+if result.multi_hand_landmarks:
+    for hand_no , hand_land_marks in enumerate(result.multi_hand_landmarks):
+        mp_drawings.draw_landmarks(
+                                    image=img,
+                                    landmark_list=hand_land_marks,
+                                    connections=mp_hands.HAND_CONNECTIONS 
+                                )
+
 
 cv2.imshow('hand_tracking',img)
 cv2.waitKey()
